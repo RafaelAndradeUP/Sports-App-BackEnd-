b35a419171b6f3e8972966107b02f199c144b6e7
Josue0H <78229953+Josue0H@users.noreply.github.com>
2021-10-29 19:01:54 -0500

    fixed server

--- src/modelos/modusuario_seguidor.js
+++ src/modelos/modusuario_seguidor.js
@@ -1,10 +1,22 @@
 const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
 
 const SUSchema =new mongoose.Schema({
-    id: {type: String, required: true, unique:true},
-    Seguidor: {type: String, required: true, unique:false},
-    Seguido: {type: String, required: true, unique:false}
-});
+    seguidor: {
+        type: ObjectId, 
+        ref: 'Usuario',
+        required: true, 
+        unique:false
+    },
+    Seguido: {
+        type: ObjectId, 
+        ref: 'Usuario',
+        required: true, 
+        unique:false
+    }
+},
+{timestamps: true}
+);
 
-const SU = mongoose.model("SU",SUSchema);
+const SU = mongoose.model("SU", SUSchema, 'Interacciones');
 module.exports= SU;
