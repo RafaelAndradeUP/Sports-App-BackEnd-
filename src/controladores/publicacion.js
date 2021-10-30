308c0f6344326e0535980c889325f27cb1a4edb4
RafaelAndradeRC <73724054+RafaelAndradeRC@users.noreply.github.com>
2021-10-29 20:09:22 -0500

    Actualización de funciones CRUD de los posts

--- src/controladores/publicacion.js
+++ src/controladores/publicacion.js
@@ -6,17 +6,21 @@ const Usuario = require('../modelos/modelousuario');
 
 const control={};
 
-control.inicio=(req,res)=>{
-    
-};
+
 control.crear=(req,res)=>{
-    res.send('Creacion');
+    let Aux=new Post({ususario:String(req.body.usuario),texto:String(req.body.texto),nlikes:0});
+    Aux.save();
+    res.send(Aux.json());
 };
 control.like=(req,res)=>{
-
+    let Aux=new PL({postId:req.body.postId, usuarioID:req.body.usuarioID});
+    Aux.save();
+    Post.updateOne({id:req.body.postId},{$inc:{nlikes:1}}).then((data)=>{console.log("Likeado")}).catch((err)=>console.log("Algo salio mal al opinar:",err));
+    res.send('Gracias, tu opinion es importante');
 };
 control.eliminar=(req,res)=>{
-    
+    Post.deleteOne({id:req.body.postId}).then((data)=>{console.log("Eliminado")}).catch((err)=>console.log("Algo salio mal al borrar:",err));
+    res.send('Post eliminado')
 };
 
 module.exports=control;
