245a56a606201852ff8cb01715f0689472addc9c
RafaelAndradeRC <73724054+RafaelAndradeRC@users.noreply.github.com>
2021-12-05 11:20:22 -0600

    Equipos de la Premier como temas
    De la Api de deportes se extrajo los nombres de la Premier para que una vez ejecutado se creen esas temáticas. #Como los temas deben ser únicos sólo se puede ejecutar una vez

---
+++ src/modelos/InsertarPremier.js
@@ -0,0 +1,27 @@
+const Tema=require('../modelos/modelotema');
+
+Tema.insertMany([
+    {tematica:"Arsenal"},
+    {tematica:"Aston Villa"},
+    {tematica:"Brentford"},
+    {tematica:"Brighton"},
+    {tematica:"Burnley"},
+    {tematica:"Chelsea"},
+    {tematica:"Crystal Palace"},
+    {tematica:"Everton"},
+    {tematica:"Leeds"},
+    {tematica:"Leicester"},
+    {tematica:"Liverpool"},
+    {tematica:"Man City"},
+    {tematica:"Man United"},
+    {tematica:"Newcastle"},
+    {tematica:"Norwich"},
+    {tematica:"Southampton"},
+    {tematica:"Tottenham"},
+    {tematica:"Watford"},
+    {tematica:"West Ham"},
+    {tematica:"Wolves"}
+]);
+
+//Link (el valor del nombre es "strTeam"):
+//https://www.thesportsdb.com/api/v1/json/2/search_all_teams.php?l=English%20Premier%20League
